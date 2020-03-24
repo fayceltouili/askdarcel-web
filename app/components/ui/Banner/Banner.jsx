36a72260f5b59736d39f1b0b60fe73f81fb4f406
Anna Kalkanis <a.kalkanis@gmail.com>
2020-03-23 21:03:12 -0700

    update banner per dereks designs (#923)
    Co-authored-by: Anna Kalkanis-Ellis <akalkanis@Annas-MacBook-Pro.local>

--- app/components/ui/Banner/Banner.jsx
+++ app/components/ui/Banner/Banner.jsx
@@ -4,15 +4,12 @@ import styles from './Banner.scss';
 export default function Banner() {
   return (
     <div className={styles.bannerContainer}>
-      <strong>CORONAVIRUS (COVID-19) UPDATE: </strong>
-      <span>
-        Many resources are operating with abnormal or reduced hours.
-        Please find the latest information on resources available during this pandemic
-        {' '}
-        <a className={styles.bannerLink} href="https://docs.google.com/document/d/1R9y8KLbU-oZTJheoqobmqg6TJxkwSjTkJvm6WywHURk/edit">here</a>
-        . We are working to incorporate active services into the SF Service Guide
-         - check back for more updates.
-      </span>
+      <strong>CORONAVIRUS COVID-19: </strong>
+      Many organizations and services have reduced hours and availability. See our new
+      {' '}
+      <a className={styles.bannerLink} href="https://docs.google.com/document/d/1R9y8KLbU-oZTJheoqobmqg6TJxkwSjTkJvm6WywHURk/edit">Resource Guide</a>
+      {' '}
+      - updated daily
     </div>
   );
 }
