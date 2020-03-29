78b239ed7d728792ba5a19a8b9dd7b8e5a760161
Anna Kalkanis-Ellis <akalkanis@Annas-MacBook-Pro.local>
2020-03-28 19:51:50 -0700

    update banner

--- app/components/ui/Banner/Banner.jsx
+++ app/components/ui/Banner/Banner.jsx
@@ -5,11 +5,11 @@ export default function Banner() {
   return (
     <div className={styles.bannerContainer}>
       <strong>CORONAVIRUS COVID-19: </strong>
-      Many organizations and services have reduced hours and availability. See our new
+      Many organizations and services have reduced hours and availability. Click
       {' '}
-      <a className={styles.bannerLink} href="https://docs.google.com/document/d/1R9y8KLbU-oZTJheoqobmqg6TJxkwSjTkJvm6WywHURk/edit">Resource Guide</a>
+      <a className={styles.bannerLink} href="https://docs.google.com/document/d/1R9y8KLbU-oZTJheoqobmqg6TJxkwSjTkJvm6WywHURk/edit">here</a>
       {' '}
-      - updated daily
+      for a helpful resource guide - updated daily.
     </div>
   );
 }
