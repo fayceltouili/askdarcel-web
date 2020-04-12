c3f05d03fdc37feee6b212fd863cd4e51d287b63
Richard Xia <richardxia@richardxia.com>
2020-04-12 14:36:41 -0700

    Remove empty modules. (#932)

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -4,7 +4,6 @@ import { connectRouter } from 'connected-react-router';
 import resource from './resourceReducer';
 import services from './serviceReducer';
 import auth from './authReducer';
-import changeRequestReducer from './changeRequestReducer';
 import popUpMessageReducer from './popUpMessageReducer';
 import forms from './formConfig';
 
@@ -12,7 +11,6 @@ import { User } from '../models';
 
 const createRootReducer = history => combineReducers({
   auth,
-  changeRequestReducer,
   forms: combineForms(forms, 'forms'),
   popUpMessage: popUpMessageReducer,
   resource,
