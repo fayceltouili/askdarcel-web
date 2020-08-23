90531f6e9ec568ebda1a3c622f1e6f7409ff9d6f
mac <mac@macs-mbp.lan>
2020-08-23 15:34:20 -0700

    fix CI errors

--- app/pages/ServiceDiscoveryResults/SearchResults/SearchResults.jsx
+++ app/pages/ServiceDiscoveryResults/SearchResults/SearchResults.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 import ReactMarkdown from 'react-markdown';
 import { get as _get } from 'lodash';
 import { connectStateResults } from 'react-instantsearch/connectors';
